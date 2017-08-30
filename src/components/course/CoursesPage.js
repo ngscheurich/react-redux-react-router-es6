0061b36caa1f6c1535a69e0844bb94577f5e5ae4
N. G. Scheurich <nick@scheurich.me>
2017-08-29 22:49:49 -0500

    Add functions to track local state changes

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,43 @@
 import React from 'react'
 
 class CoursesPage extends React.Component {
+  constructor (props, context) {
+    super(props, context)
+
+    this.state = {
+      course: { title: '' }
+    }
+
+    this.onTitleChange = this.onTitleChange.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
+  }
+
+  onTitleChange (event) {
+    let course = this.state.course
+    course.title = event.target.value
+    this.setState({ course: course })
+  }
+
+  onClickSave () {
+    console.log(`Saving ${this.state.course.title}`)
+  }
+
   render () {
     return (
-      <div className="container-fluid pt-3">
+      <div className="container-fluid mt-4">
         <h1>Courses</h1>
+
+        <input
+          placeholder="Add course"
+          className="form-control"
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+        <input
+          className="btn btn-primary"
+          type="submit"
+          onClick={this.onClickSave}
+          value="Save" />
       </div>
     )
   }
