00b406a1620c95273534407d6d4a600d0d7fcc05
N. G. Scheurich <nick@scheurich.me>
2017-09-05 11:55:00 -0500

    Add link to add course

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -5,14 +5,32 @@ import * as courseActions from '../../actions/courseActions'
 import CourseList from './CourseList'
 
 class CoursesPage extends React.Component {
+  constructor (props, context) {
+    super(props, context)
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this)
+  }
+
   courseRow (course, i) {
     return <div key={i}>{course.title}</div>
+  }
+
+  redirectToAddCoursePage (event) {
+    event.preventDefault()
+    this.context.router.push('/course')
   }
 
   render () {
     return (
       <div className="container-fluid pt-4">
-        <h1 className="mb-3">Courses</h1>
+        <div className="d-flex justify-content-between align-items-center mb-3">
+          <h1>Courses</h1>
+          <a
+            href="/course"
+            className="btn btn-primary"
+            onClick={this.redirectToAddCoursePage}>
+            Add Course
+          </a>
+        </div>
         <CourseList courses={this.props.courses} />
       </div>
     )
@@ -22,6 +40,10 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
+}
+
+CoursesPage.contextTypes = {
+  router: PropTypes.object
 }
 
 function mapStateToProps (state, ownProps) {
