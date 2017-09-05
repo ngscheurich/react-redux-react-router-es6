0c1c7b38b0248a62d89b0dcb2f88634771a23dca
N. G. Scheurich <nick@scheurich.me>
2017-09-05 11:52:13 -0500

    Add loading indicator

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Link, IndexLink } from 'react-router'
 import logo from './logo.svg'
+import puff from './puff.svg'
 
-const Header = () => {
+const Header = ({loading}) => {
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
       <IndexLink to="/" className="navbar-brand">
-        <img src={logo} width="30" height="30" alt="Pluralsight Administration logo" />
+        <img src={loading ? puff : logo} width="30" height="30" alt={loading ? 'App logo' : 'Loading...'} />
       </IndexLink>
       <div className="collapse navbar-collapse">
         <div className="navbar-nav">
@@ -17,6 +18,10 @@ const Header = () => {
       </div>
     </nav>
   )
+}
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
 }
 
 export default Header
